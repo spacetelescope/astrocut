b54cc5d8d91c973b46026e03569f87f575a65ce9
Jacob Matuskey <jmatuskey@stsci.edu>
2018-10-05 11:38:58 -0400

    attempt at regenerating from cookiecutter

--- astrocut/__init__.py
+++ astrocut/__init__.py
@@ -20,4 +20,4 @@ if sys.version_info < tuple((int(val) for val in __minimum_python_version__.spli
 
 if not _ASTROPY_SETUP_:
     # For egg_info test builds to pass, put package imports here.
-    pass
+    from .example_mod import *
