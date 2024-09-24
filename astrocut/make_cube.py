890ee8751ab0bf4177bf683741d665a21e53ab94
Sam Bianco <70121323+snbianco@users.noreply.github.com>
2024-09-24 10:13:03 -0400

    Bugfix for implementation and test for invalid file type input in make_cube() (#129)

--- astrocut/make_cube.py
+++ astrocut/make_cube.py
@@ -19,8 +19,8 @@ if (version_info >= (3, 8)) and (platform != "win32"):
     from mmap import MADV_SEQUENTIAL
 
 __all__ = ['CubeFactory', 'TicaCubeFactory']
-ERROR_MSG = "One or more incorrect file types were input. Please input TICA FFI files when using\
-                   ``TicaCubeFactory``, and SPOC FFI files when using ``CubeFactory``."
+ERROR_MSG = ("One or more incorrect file types were input. Please input TICA FFI files when using "
+             "``TicaCubeFactory``, and SPOC FFI files when using ``CubeFactory``.")
 
 
 class CubeFactory():
@@ -66,7 +66,10 @@ class CubeFactory():
 
             ffi_data = fits.open(ffi, mode='denywrite', memmap=True)
             
-            start_times[i] = ffi_data[1].header.get(self.time_keyword)
+            try:
+                start_times[i] = ffi_data[1].header.get(self.time_keyword)
+            except IndexError:
+                raise ValueError(ERROR_MSG)
 
             if image_shape is None:  # Only need to fill this once
                 image_shape = ffi_data[1].data.shape
@@ -87,10 +90,7 @@ class CubeFactory():
 
         # Working out the block size and number of blocks needed for writing the cube
         # without using too much memory
-        try:
-            slice_size = image_shape[1] * len(self.file_list) * 2 * 4  # in bytes (float32)
-        except IndexError:
-            raise ValueError(ERROR_MSG)
+        slice_size = image_shape[1] * len(self.file_list) * 2 * 4  # in bytes (float32)
         max_block_size = int((self.max_memory * 1e9)//slice_size)
         
         self.num_blocks = int(image_shape[0]/max_block_size + 1)
