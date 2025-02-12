b29e8429617a1cbda3af55cf8a69b47219a12f61
Sam Bianco <sbianco@stsci.edu>
2025-02-12 15:41:35 -0500

    small typing changes

--- astrocut/image_cutout.py
+++ astrocut/image_cutout.py
@@ -111,7 +111,7 @@ class ImageCutout(Cutout, ABC):
         """
         # Validate the stretch parameter
         valid_stretches = ['asinh', 'sinh', 'sqrt', 'log', 'linear']
-        if stretch and not isinstance(stretch, str) or stretch.lower() not in valid_stretches:
+        if not isinstance(stretch, str) or stretch.lower() not in valid_stretches:
             raise InvalidInputError(f'Stretch {stretch} is not recognized. Valid options are {valid_stretches}.')
         stretch = stretch.lower()
 
