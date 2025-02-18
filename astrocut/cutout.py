ab714a61df55943fa78db2b8fb0b2de70dafcad6
Sam Bianco <sbianco@stsci.edu>
2025-02-17 19:13:02 -0500

    pass in coordinates from asdf_cut as string

--- astrocut/cutout.py
+++ astrocut/cutout.py
@@ -55,9 +55,7 @@ class Cutout(ABC):
         self._input_files = input_files
 
         # Get coordinates as a SkyCoord object
-        if isinstance(coordinates, tuple):
-            coordinates = SkyCoord(*coordinates, unit='deg')
-        elif not isinstance(coordinates, SkyCoord):
+        if not isinstance(coordinates, SkyCoord):
             coordinates = SkyCoord(coordinates, unit='deg')
         self._coordinates = coordinates
         log.debug('Coordinates: %s', self._coordinates)
