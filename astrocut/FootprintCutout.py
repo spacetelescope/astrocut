a8d20bf949b8f7a461d959f46535cc2b1fcc9a82
Sam Bianco <sbianco@stsci.edu>
2025-03-11 11:45:48 -0400

    Implementation, comments, tests

---
+++ astrocut/FootprintCutout.py
@@ -0,0 +1,257 @@
+import json
+from abc import ABC, abstractmethod
+from threading import Lock
+from typing import List, Tuple, Union
+
+import astropy.units as u
+import fsspec
+import numpy as np
+from astropy.coordinates import SkyCoord
+from astropy.table import Table, Column
+from cachetools import TTLCache, cached
+from spherical_geometry.polygon import SphericalPolygon
+
+from .Cutout import Cutout
+
+FFI_TTLCACHE = TTLCache(maxsize=10, ttl=900)  # Cache for FFI footprint files
+
+
+class FootprintCutout(Cutout, ABC):
+    """
+    Abstract class that creates cutouts from data files hosted on the S3 cloud.
+
+    Args
+    ----
+    coordinates : str | `~astropy.coordinates.SkyCoord`
+        Coordinates of the center of the cutout.
+    cutout_size : int | array | list | tuple | `~astropy.units.Quantity`
+        Size of the cutout array.
+    fill_value : int | float
+        Value to fill the cutout with if the cutout is outside the image.
+    limit_rounding_method : str
+        Method to use for rounding the cutout limits. Options are 'round', 'ceil', and 'floor'.
+    sequence : int | list | None
+        Default None. Sequence(s) from which to generate cutouts. Can provide a single
+        sequence number as an int or a list of sequence numbers. If not specified, 
+        cutouts will be generated from all sequences that contain the cutout.
+    verbose : bool
+        If True, log messages are printed to the console.
+
+    Methods
+    -------
+    _get_ffis()
+        Fetches footprints for Full Frame Images (FFIs) from the S3 cloud.
+    _get_files_from_cone_results(cone_results)
+        Returns the files that contain the cutout.
+    cutout()
+        Fetch the cloud files that contain the cutout and generate the cutouts.
+    _s_region_to_polygon(s_region)
+        Converts the s_region string to a SphericalPolygon object.
+    _ffi_intersect(ffi_list, polygon)
+        Vectorizes the intersects_polygon function.
+    ra_dec_crossmatch(all_ffis, coordinates, cutout_size, arcsec_per_px)
+        Returns the FFIs whose footprints overlap with the cutout.
+    """
+
+    def __init__(self, coordinates: Union[SkyCoord, str], 
+                 cutout_size: Union[int, np.ndarray, u.Quantity, List[int], Tuple[int]] = 25,
+                 fill_value: Union[int, float] = np.nan, limit_rounding_method: str = 'round', 
+                 sequence: Union[int, List[int], None] = None, verbose: bool = False):
+        super().__init__([], coordinates, cutout_size, fill_value, limit_rounding_method, verbose)
+
+        # Assigning other attributes
+        if isinstance(sequence, int):
+            sequence = [sequence]  # Convert to list
+        self._sequence = sequence
+
+        # Populate these in child classes 
+        self._s3_footprint_cache = None  # S3 URI to footprint cache file
+        self._arcsec_per_px = None  # Number of arcseconds per pixel in an image
+    
+    @cached(cache=FFI_TTLCACHE, lock=Lock())
+    def _get_ffis(self) -> Table:
+        """
+        Fetches footprints for Full Frame Images (FFIs) from S3. The resulting
+        table contains each (FFI) and a 'polygon' column that describes the image's footprints as polygon points
+        and vectors.
+
+        Returns
+        -------
+        ffis : `~astropy.table.Table`
+            Table containing information about FFIs and their footprints.
+        """
+        # Open footprint file with fsspec
+        with fsspec.open(self._s3_footprint_cache, s3={'anon': True}) as f:
+            ffis = json.load(f)
+
+        # Compute spherical polygons
+        ffis['polygon'] = self._s_region_to_polygon(ffis['s_region'])
+
+        # Convert to Astropy table
+        ffis = Table(ffis)
+
+        return ffis
+    
+    @abstractmethod
+    def _get_files_from_cone_results(self, cone_results: Table) -> dict:
+        """
+        Converts a `~astropy.table.Table` of cone search results to a list of dictionaries containing
+        metadata for each cloud file that intersects with the cutout.
+        
+        This method is abstract and should be implemented in subclasses.
+        """
+        pass
+
+    @abstractmethod
+    def cutout(self):
+        """
+        Generate cutouts from the cloud files that contain the cutout's footprint.
+
+        This method is abstract and should be implemented in subclasses.
+        """
+        pass
+
+    @staticmethod
+    def _s_region_to_polygon(s_region: Column) -> Column:
+        """
+        Returns a column of `~spherical_geometry.polygon.SphericalPolygon` objects from a column of
+        s_region strings.
+
+        Parameters
+        ----------
+        s_region : `~astropy.table.Column`
+            Column containing the s_region string. Example input: 'POLYGON 229.80771900 -75.17048500 
+            241.67788000 -63.95992300 269.94872000 -64.39276400 277.87862300 -75.57754400'
+
+        Returns
+        -------
+        polygon : `~astropy.table.Column`
+            Column containing `~spherical_geometry.polygon.SphericalPolygon` objects representing each s_region.
+        """
+        def ind_sregion_to_polygon(s_reg):
+            """
+            Helper function to convert s_region string to a `~spherical_geometry.polygon.SphericalPolygon` object.
+
+            Parameters
+            ----------
+            s_reg : str
+                A string defining a spatial region, expected to be in the 'POLYGON' format.
+
+            Returns
+            -------
+            `~spherical_geometry.polygon.SphericalPolygon`
+                A SphericalPolygon object created from the provided coordinates.
+            
+            Raises
+            ------
+            ValueError
+                If the S_REGION type is not 'POLYGON'.
+            
+            """
+            # Split input string into individual components
+            sr_list = s_reg.strip().split()
+
+            # Extract the region type (first element of list)
+            reg_type = sr_list[0].upper()
+
+            if reg_type == 'POLYGON':
+                # Extract RA and Dec values
+                # RAs are at odd indices
+                ras = np.array(sr_list[1::2], dtype=float)
+
+                # Convert negative RAs to the 0-360 range
+                ras[ras < 0] = ras[ras < 0] + 360
+
+                # Decs are at even indices
+                decs = np.array(sr_list[2::2], dtype=float)
+
+                # Create SphericalPolygon object
+                return SphericalPolygon.from_radec(ras, decs)
+            else:
+                raise ValueError(f'Unsupported s_region type: {reg_type}.')
+
+        return np.vectorize(ind_sregion_to_polygon)(s_region)
+    
+    @staticmethod
+    def _ffi_intersect(ffi_list: Table, polygon: SphericalPolygon) -> np.ndarray:
+        """
+        Vectorizing the spherical_coordinate intersects_polygon function.
+
+        Parameters
+        ----------
+        ffi_list : `~astropy.table.Table`
+            Table containing information about FFIs and their footprints.
+        polygon : `~spherical_geometry.polygon.SphericalPolygon`
+            SphericalPolygon object representing the cutout's footprint.
+
+        Returns
+        -------
+        intersect : `~numpy.ndarray`
+            Boolean array indicating whether each FFI intersects with the cutout.
+        """
+        def single_intersect(ffi, polygon):
+            return ffi.intersects_poly(polygon)
+
+        return np.vectorize(single_intersect)(ffi_list['polygon'], polygon)
+    
+    @staticmethod
+    def ra_dec_crossmatch(all_ffis: Table, coordinates: SkyCoord, cutout_size, 
+                          arcsec_per_px: int = 21) -> Table:
+        """
+        Returns the Full Frame Images (FFIs) whose footprints overlap with a cutout of a given position and size.
+
+        Parameters
+        ----------
+        all_ffis : `~astropy.table.Table`
+            Table of FFIs to crossmatch with the cutout.
+        coordinates : str or `astropy.coordinates.SkyCoord` object
+            The position around which to cutout.
+            It may be specified as a string ("ra dec" in degrees)
+            or as the appropriate `~astropy.coordinates.SkyCoord` object.
+        cutout_size : int, array-like, `~astropy.units.Quantity`
+            The size of the cutout array. If ``cutout_size``
+            is a scalar number or a scalar `~astropy.units.Quantity`,
+            then a square cutout of ``cutout_size`` will be created.  If
+            ``cutout_size`` has two elements, they should be in ``(ny, nx)``
+            order.  Scalar numbers in ``cutout_size`` are assumed to be in
+            units of pixels. `~astropy.units.Quantity` objects must be in pixel or
+            angular units.
+        arcsec_per_px : int, optional
+            Default 21. The number of arcseconds per pixel in an image. Used to determine
+            the footprint of the cutout. Default is the number of arcseconds per pixel in
+            a TESS image.
+
+        Returns
+        -------
+        matching_ffis : `~astropy.table.Table`
+            Table containing information about FFIs whose footprints overlap those of the cutout.
+        """
+        # Convert coordinates to SkyCoord
+        if not isinstance(coordinates, SkyCoord):
+            coordinates = SkyCoord(coordinates, unit='deg')
+        ra, dec = coordinates.ra, coordinates.dec
+
+        # Parse cutout size
+        cutout_size = Cutout._parse_size_input(cutout_size)
+
+        # Create polygon for intersection
+        # Convert dimensions from pixels to arcseconds and divide by 2 to get offset from center
+        ra_offset = ((cutout_size[0] * arcsec_per_px) / 2) * u.arcsec
+        dec_offset = ((cutout_size[1] * arcsec_per_px) / 2) * u.arcsec
+
+        # Calculate RA and Dec boundaries
+        ra_bounds = [ra - ra_offset, ra + ra_offset]
+        dec_bounds = [dec - dec_offset, dec + dec_offset]
+
+        # Get RA and Dec for four corners of rectangle
+        ras = [ra_bounds[0].value, ra_bounds[1].value, ra_bounds[1].value, ra_bounds[0].value]
+        decs = [dec_bounds[0].value, dec_bounds[0].value, dec_bounds[1].value, dec_bounds[1].value]
+
+        # Create SphericalPolygon for comparison
+        cutout_fp = SphericalPolygon.from_radec(ras, decs, center=(ra, dec))
+
+        # Find indices of FFIs that intersect with the cutout
+        ffi_inds = np.vectorize(lambda ffi: ffi.intersects_poly(cutout_fp))(all_ffis['polygon'])
+        ffi_inds = FootprintCutout._ffi_intersect(all_ffis, cutout_fp)
+
+        return all_ffis[ffi_inds]
---
+++ astrocut/tests/test_TessFootprintCutout.py
@@ -0,0 +1,220 @@
+from pathlib import Path
+import pytest
+import re
+
+from astropy.coordinates import SkyCoord
+from astropy.io import fits
+from astropy.table import Table
+from spherical_geometry.polygon import SphericalPolygon
+
+from ..CubeCutout import CubeCutout
+from ..exceptions import InvalidInputError, InvalidQueryError
+from ..footprint_cutouts import cube_cut_from_footprint
+from ..TessFootprintCutout import TessFootprintCutout
+from ..TessCubeCutout import TessCubeCutout
+
+
+@pytest.fixture
+def cutout_size():
+    """Fixture to return the cutout size"""
+    return 5
+
+
+@pytest.fixture
+def coordinates():
+    """Fixture to return the coordinates at the center of the images"""
+    return SkyCoord('350 -80', unit='deg')
+
+
+def check_output_tpf(tpf, ffi_type, sequences=[], cutout_size=5):
+    """Helper function to check the validity of output cutout files"""
+    tpf_table = tpf[1].data
+
+    # SPOC cutouts have 1 extra columns in EXT 1
+    ncols = 12 if ffi_type == 'SPOC' else 11
+    assert len(tpf_table.columns) == ncols
+    assert tpf_table[0]['FLUX'].shape == (cutout_size, cutout_size)
+
+    # Check that sector matches a provided sequence
+    if sequences:
+        assert tpf[0].header['SECTOR'] in sequences
+
+    # Close TPF
+    tpf.close()
+
+
+def test_s_region_to_polygon_unsupported_region():
+    """Test that ValueError is raised if s_region is not a polygon"""
+    s_region = 'CIRCLE'
+    err = f'Unsupported s_region type: {s_region}'
+    with pytest.raises(ValueError, match=err):
+        TessFootprintCutout._s_region_to_polygon(s_region)
+
+
+@pytest.mark.parametrize("lon, lat, center, expected", [
+    ((345, 355, 355, 345), (-15, -15, -5, -5), (350, -10), True),  # intersecting
+    ((335, 345, 345, 335), (-15, -15, -5, -5), (340, -10), False),  # non-intersecting
+    ((340, 350, 350, 340), (-15, -15, -5, -5), (345, -10), True),  # edge object that intersects
+    ((340, 349, 349, 340), (-15, -15, -5, -5), (345, -10), False),  # edge object that does not intersect
+])
+def test_ffi_intersect(lon, lat, center, expected):
+    """Test that FFI intersection with cutout outputs proper results."""
+    # SphericalPolygon object for cutout
+    cutout_sp = SphericalPolygon.from_radec(lon=(350, 10, 10, 350),
+                                            lat=(-10, -10, 10, 10),
+                                            center=(0, 0))
+
+    # Create a SphericalPolygon with the parametrized lon, lat, and center
+    polygon = SphericalPolygon.from_radec(lon=lon, lat=lat, center=center)
+
+    # Create a table with this polygon
+    polygon_table = Table(names=['polygon'], dtype=[SphericalPolygon])
+    polygon_table['polygon'] = [polygon]
+
+    # Perform the intersection check
+    intersection = TessFootprintCutout._ffi_intersect(polygon_table, cutout_sp)
+
+    # Assert the intersection result matches the expected value
+    assert intersection.value[0] == expected
+
+
+@pytest.mark.parametrize('ffi_type', ['SPOC', 'TICA'])
+def test_tess_footprint_cutout(cutout_size, caplog, ffi_type):
+    """Test that a single data cube is created for a given sequence"""
+    cutout = TessFootprintCutout('130 30', cutout_size, product=ffi_type, sequence=44, verbose=True)
+
+    # Check cutouts attribute
+    cutouts = cutout.cutouts
+    assert len(cutouts) == 1
+    assert isinstance(cutouts, list)
+    assert isinstance(cutouts[0], CubeCutout.CubeCutoutInstance)
+    assert cutouts[0].shape[1:] == (5, 5)
+
+    # Check cutouts_by_file attribute
+    cutouts_by_file = cutout.cutouts_by_file
+    assert len(cutouts_by_file) == 1
+    assert isinstance(cutouts_by_file, dict)
+    assert isinstance(list(cutouts_by_file.values())[0], CubeCutout.CubeCutoutInstance)
+
+    # Check tpf_cutouts attribute
+    tpf_cutouts = cutout.tpf_cutouts
+    assert len(tpf_cutouts) == 1
+    assert isinstance(tpf_cutouts, list)
+    assert isinstance(tpf_cutouts[0], fits.HDUList)
+    check_output_tpf(tpf_cutouts[0], ffi_type, [44])
+
+    # Check tpf_cutouts_by_file
+    tpf_cutouts_by_file = cutout.tpf_cutouts_by_file
+    tpf_cutout = list(tpf_cutouts_by_file.values())[0]
+    assert len(tpf_cutouts_by_file) == 1
+    assert isinstance(tpf_cutouts_by_file, dict)
+    assert isinstance(tpf_cutout, fits.HDUList)
+    check_output_tpf(tpf_cutout, ffi_type, [44])
+
+    # Check tess_cube attribute
+    tess_cube = cutout.tess_cube_cutout
+    assert isinstance(tess_cube, TessCubeCutout)
+    
+    # Assert that messages were printed
+    captured = caplog.text
+    assert 'Coordinates:' in captured
+    assert 'Cutout size: [5 5]' in captured
+    assert re.search(r'Found \d+ footprint files.', captured)
+    assert re.search(r'Filtered to \d+ footprints for sequences: 44', captured)
+    assert re.search(r'Found \d+ matching files.', captured)
+    assert 'Generating cutouts...' in captured
+
+    # Check that _extract_sequence_information works correctly
+    # Should return empty dict if sector name does not match product
+    sector_name = 'hlsp_tica_s' if ffi_type == 'SPOC' else 'tess_s'
+    sector_name += '0044-4-1'
+    info = cutout._extract_sequence_information(sector_name)
+    assert info == {}
+
+
+def test_tess_footprint_cutout_multi_sequence(coordinates, cutout_size):
+    """Test that a cube is created for each sequence when multiple are provided"""
+    sequences = [1, 13]
+    cutout = TessFootprintCutout(coordinates, cutout_size, product='SPOC', sequence=sequences)
+    cutout_tpfs = cutout.tpf_cutouts
+    assert len(cutout_tpfs) == 2
+
+    for tpf in cutout_tpfs:
+        check_output_tpf(tpf, 'SPOC', sequences)
+
+
+def test_tess_footprint_cutout_all_sequences(coordinates, cutout_size):
+    """Test that cubes are created for all sequences that intersect the cutout"""
+    # Create cutouts for all possible sequences
+    cutout = TessFootprintCutout(coordinates, cutout_size, product='SPOC')
+    cutout_tpfs = cutout.tpf_cutouts
+    assert len(cutout_tpfs) >= 5
+
+    # Crossmatch to get sectors that contain cutout
+    all_ffis = cutout._get_ffis()
+    cone_results = TessFootprintCutout.ra_dec_crossmatch(all_ffis, '350 -80', cutout_size, 21)
+    seq_list = cutout._create_sequence_list(cone_results)
+    sequences = [int(seq['sector']) for seq in seq_list]
+
+    # Assert non-empty results
+    assert len(seq_list) == len(cutout_tpfs)
+    for tpf in cutout_tpfs:
+        check_output_tpf(tpf, 'SPOC', sequences)
+
+
+def test_tess_footprint_cutout_write_as_tpf(coordinates, cutout_size, tmpdir):
+    """Test that TPF files are written to disk"""
+    cutout = TessFootprintCutout(coordinates, cutout_size, sequence=[1, 13])
+    paths = cutout.write_as_tpf(output_dir=tmpdir)
+
+    for cutout_path in paths:
+        path = Path(cutout_path)
+        assert path.exists()
+        assert path.suffix == '.fits'
+
+        # Check that file can be opened
+        with fits.open(path) as hdu:
+            hdu.info()
+
+
+def test_tess_footprint_cutout_invalid_sequence(coordinates, cutout_size):
+    """Test that InvalidQueryError is raised if sequence does not have cube files"""
+    err = 'No files were found for sequences: -1'
+    with pytest.raises(InvalidQueryError, match=err):
+        TessFootprintCutout(coordinates, cutout_size, sequence=-1)
+
+
+def test_tess_footprint_cutout_outside_coords(coordinates, cutout_size):
+    """Test that InvalidQueryError is raised if coordinates are not found in sequence"""
+    err = 'The given coordinates were not found within the specified sequence(s).'
+    with pytest.raises(InvalidQueryError, match=re.escape(err)):
+        TessFootprintCutout(coordinates, cutout_size, sequence=2)
+
+
+def test_tess_footprint_cutout_invalid_product(coordinates, cutout_size):
+    """Test that InvalidQueryError is raised if an invalid product is given"""
+    err = 'Product for TESS cube cutouts must be either "SPOC" or "TICA".'
+    with pytest.raises(InvalidInputError, match=err):
+        TessFootprintCutout(coordinates, cutout_size, product='invalid')
+        
+
+def test_cube_cut_from_footprint(coordinates, cutout_size, tmpdir):
+    """Test that data cube is cut from FFI file using parallel processing"""
+    # Writing to memory, should return cutouts as memory objects
+    cutouts = cube_cut_from_footprint(coordinates, 
+                                      cutout_size,
+                                      sequence=13,
+                                      memory_only=True)
+    assert len(cutouts) == 1
+    assert isinstance(cutouts, list)
+    assert isinstance(cutouts[0], fits.HDUList)
+
+    # Writing to disk, should return cutout filepaths
+    cutouts = cube_cut_from_footprint(coordinates, 
+                                      cutout_size,
+                                      sequence=13,
+                                      output_dir=tmpdir)
+    assert len(cutouts) == 1
+    assert isinstance(cutouts, list)
+    assert isinstance(cutouts[0], str)
+    assert tmpdir in cutouts[0]
